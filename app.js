b3653af000f1661fc35051ca5783ac0bbcea27fd
Nonolism <ruanvitormoreno@outlook.com>
2024-08-08 17:54:15 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+let campoQuantidade = document.getElementById('quantidade')
+let campoInicio = document.getElementById('de')
+let campoFim = document.getElementById('ate')
+
+
+function sortear() {
+    let quantidade = campoQuantidade.value
+    let inicio = campoInicio.value
+    let fim = campoFim.value
+    console.log(quantidade,inicio,fim)
+}
+
+
