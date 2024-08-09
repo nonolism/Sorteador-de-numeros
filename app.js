5c83495828718ba5e4d8580323c237ed623cfcf0
Nonolism <ruanvitormoreno@outlook.com>
2024-08-09 19:02:09 -0300

    Criar função para gerar número

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-let campoQuantidade = document.getElementById('quantidade')
-let campoInicio = document.getElementById('de')
-let campoFim = document.getElementById('ate')
+let campoQuantidade = document.getElementById('quantidade');
+let campoInicio = document.getElementById('de');
+let campoFim = document.getElementById('ate');
+let botaoReiniciar = document.getElementById('btn-reiniciar');
 
-
-function sortear() {
-    let quantidade = campoQuantidade.value
-    let inicio = campoInicio.value
-    let fim = campoFim.value
-    console.log(quantidade,inicio,fim)
+function gerarNumeroEntreInicioEFim(inicio,fim){
+    return Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
 }
 
+function sortear() {
+    let quantidade = parseInt(campoQuantidade.value);
+    let inicio = parseInt(campoInicio.value);
+    let fim = parseInt(campoFim.value);
 
+    console.log(gerarNumeroEntreInicioEFim(inicio,fim));
+}
