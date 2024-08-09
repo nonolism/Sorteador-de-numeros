80488d1a30fad1d2c34356b06d5a10219ce1f259
Nonolism <ruanvitormoreno@outlook.com>
2024-08-09 20:00:04 -0300

    Criar função para alteração da mensagem de resultado

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let campoQuantidade = document.getElementById('quantidade');
 let campoInicio = document.getElementById('de');
 let campoFim = document.getElementById('ate');
 let botaoReiniciar = document.getElementById('btn-reiniciar');
+let containerResultado = document.getElementById('resultado')
+let mensagemResultado = containerResultado.querySelector('.texto__paragrafo')
 
 function gerarNumeroEntreInicioEFim(inicio,fim){
     return Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
@@ -16,10 +18,19 @@ function gerarVariosNumeros(quantidade,inicio,fim){
     }
     return listaDeNumerosGerados
 }
+
+function alterarMensagemResultado(numerosSorteados) {
+    if (numerosSorteados.length != 0 ){
+        let textoResultado = `Números sorteados: ${numerosSorteados}`;
+        mensagemResultado.innerHTML = textoResultado;
+    } else {
+        mensagemResultado.innerHTML = 'Números sorteados:  nenhum dessa vez';
+    }
+}
 function sortear() {
     let quantidade = parseInt(campoQuantidade.value);
     let inicio = parseInt(campoInicio.value);
     let fim = parseInt(campoFim.value);
     let numerosSorteados = gerarVariosNumeros(quantidade,inicio,fim);
-    console.log(numerosSorteados);
+    alterarMensagemResultado(numerosSorteados);
 }
