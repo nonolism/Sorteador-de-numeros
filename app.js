5216fad0ed9ae0f5dc7d51866eee57b24dc91ae3
Nonolism <ruanvitormoreno@outlook.com>
2024-08-09 19:17:48 -0300

    Criar função para gerar varios números

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ function gerarNumeroEntreInicioEFim(inicio,fim){
     return Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
 }
 
+function gerarVariosNumeros(quantidade,inicio,fim){
+    let contador = 0;
+    let listaDeNumerosGerados = [];
+    while (contador < quantidade){
+        listaDeNumerosGerados.push(gerarNumeroEntreInicioEFim(inicio,fim))
+        contador++
+    }
+    return listaDeNumerosGerados
+}
 function sortear() {
     let quantidade = parseInt(campoQuantidade.value);
     let inicio = parseInt(campoInicio.value);
     let fim = parseInt(campoFim.value);
-
-    console.log(gerarNumeroEntreInicioEFim(inicio,fim));
+    let numerosSorteados = gerarVariosNumeros(quantidade,inicio,fim);
+    console.log(numerosSorteados);
 }
