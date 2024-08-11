ee6c569129ebb1f05afa31797a3c4e195e0cd1e4
Nonolism <ruanvitormoreno@outlook.com>
2024-08-10 22:19:58 -0300

    Criar função reiniciar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let campoQuantidade = document.getElementById('quantidade');
 let campoInicio = document.getElementById('de');
 let campoFim = document.getElementById('ate');
 let botaoReiniciar = document.getElementById('btn-reiniciar');
-let mensagemResultado = document.getElementById('resultado').querySelector('.texto__paragrafo')
+let mensagemResultado = document.getElementById('resultado').querySelector('.texto__paragrafo');
 
 function gerarNumeroEntreInicioEFim(inicio,fim){
     return Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
@@ -12,24 +12,37 @@ function gerarVariosNumeros(quantidade,inicio,fim){
     let contador = 0;
     let listaDeNumerosGerados = [];
     while (contador < quantidade){
-        listaDeNumerosGerados.push(gerarNumeroEntreInicioEFim(inicio,fim))
-        contador++
+        listaDeNumerosGerados.push(gerarNumeroEntreInicioEFim(inicio,fim));
+        contador++;
     }
-    return listaDeNumerosGerados
+    return listaDeNumerosGerados;
 }
 
-function alterarMensagemResultado(numerosSorteados) {
+function alterarMensagemResultado(numerosSorteados = []) {
+    let mensagem = 'Números sorteados:  nenhum até agora';
     if (numerosSorteados.length != 0 ){
-        let textoResultado = `Números sorteados: ${numerosSorteados}`;
-        mensagemResultado.innerHTML = textoResultado;
+        mensagem = `Números sorteados: ${numerosSorteados}`;
+        mensagemResultado.innerHTML = mensagem;
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
     } else {
-        mensagemResultado.innerHTML = 'Números sorteados:  nenhum dessa vez';
+        mensagemResultado.innerHTML = mensagem;
     }
 }
+
 function sortear() {
     let quantidade = parseInt(campoQuantidade.value);
     let inicio = parseInt(campoInicio.value);
     let fim = parseInt(campoFim.value);
     let numerosSorteados = gerarVariosNumeros(quantidade,inicio,fim);
     alterarMensagemResultado(numerosSorteados);
+}
+
+function reiniciar(){
+    campoQuantidade.value = null;
+    campoInicio.value = null;
+    campoFim.value = null;
+    alterarMensagemResultado();
+    botaoReiniciar.classList.remove('container__botao');
+    botaoReiniciar.classList.add('container__botao-desabilitado');
 }
